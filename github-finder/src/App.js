129b3d0c62521d6fc7d3abce04b1348e9e98b64b
Vali-Mandeal <51697555+Vali-Mandeal@users.noreply.github.com>
2020-03-15 12:47:18 +0200

    2.9: Expressions & Conditionals in JSX

--- github-finder/src/App.js
+++ github-finder/src/App.js
@@ -2,10 +2,16 @@ import React, { Component } from 'react';
 import './App.css';
 
 class App extends Component {
+  foo = () => 'Bars';
+
   render() {
+    const name = 'John doe';
+    const loading = false;
+    const showName = false;
+
     return (
       <div className='App'>
-        <h1>Hello from React</h1>
+        {loading ? <h4>Loading...</h4> : <h1>Hello {showName && name}</h1>}
       </div>
     );
   }
