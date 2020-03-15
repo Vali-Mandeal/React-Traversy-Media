7285e3621fbf6ef9040305c561d35fbf594c3ac0
Vali-Mandeal <51697555+Vali-Mandeal@users.noreply.github.com>
2020-03-15 15:58:23 +0200

    3.16: Environent Variables in React

--- github-finder/src/App.js
+++ github-finder/src/App.js
@@ -13,12 +13,12 @@ class App extends Component {
   };
 
   async componentDidMount() {
-    this.setState({loading: true});
+    this.setState({ loading: true });
 
-    const res = await axios.get('https://api.github.com/users');
-
+    const res = await axios.get(
+      `https://api.github.com/users?client_id=${process.env.REACT_APP_GITHUB_CLIENT_ID}&client_secret=${process.env.REACT_APP_GITHUB_CLIENT_SECRET}`
+    );
     this.setState({ users: res.data, loading: false });
-    
   }
 
   render() {
@@ -26,7 +26,7 @@ class App extends Component {
       <div className='App'>
         <Navbar />
         <div className='container'>
-          <Users loading={this.state.loading} users={this.state.users}/>
+          <Users loading={this.state.loading} users={this.state.users} />
         </div>
       </div>
     );
