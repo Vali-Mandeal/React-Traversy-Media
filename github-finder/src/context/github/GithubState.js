8a96a3d5cc5c639dfb11e5f8bb9dccea6139bc07
Vali-Mandeal <51697555+Vali-Mandeal@users.noreply.github.com>
2020-03-18 01:52:48 +0200

    5.30 Create Reducer & Actions

--- github-finder/src/context/github/GithubState.js
+++ github-finder/src/context/github/GithubState.js
@@ -1,6 +1,6 @@
 import React, { useReducer } from 'react';
 import axios from 'axios';
-import githubContext from './githubContext';
+import GithubContext from './githubContext';
 import githubReducer from './githubReducer';
 import {
   SEARCH_USERS,
@@ -22,7 +22,18 @@ const GithubState = props => {
   const [state, dispatch] = useReducer(githubReducer, initialState);
 
   // search users
+  const searchUsers = async text => {
+    setLoading();
 
+    const res = await axios.get(
+      `https://api.github.com/search/users?q=${text}&client_id=${process.env.REACT_APP_GITHUB_CLIENT_ID}&client_secret=${process.env.REACT_APP_GITHUB_CLIENT_SECRET}`
+    );
+
+    dispatch({
+      type: SEARCH_USERS,
+      payload: res.data.items
+    });
+  };
   // get user
 
   // get repos
@@ -30,18 +41,20 @@ const GithubState = props => {
   // clear users
 
   // set loading
+  const setLoading = () => dispatch({ type: SET_LOADING });
 
   return (
-    <githubContext.Provider
+    <GithubContext.Provider
       value={{
         users: state.users,
         user: state.user,
         repos: state.repos,
-        loading: state.loading
+        loading: state.loading,
+        searchUsers
       }}
     >
       {props.children}
-    </githubContext.Provider>
+    </GithubContext.Provider>
   );
 };
 
